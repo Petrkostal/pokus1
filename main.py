d8aa6bc63162919e102e19be635dc61a10d8cfc0
Your Name <you@example.com>
2025-07-04 21:02:21 +0200

    pridani databaze a cursoru

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-import Flask
+import sqlite3
+
+connection = sqlite3.connect('data_colect')
+cur = connection.cursor()
+cur.execute("drop table OHC_data")
+cur.execute("""create table OHC_data(
+           id integer primary key autoincrement
+            ,symbol text not null
+            ,last_price real not null
+            ,last_time datetime not null
+             )""")
+
+for i in range(5):
+    cur.execute(f'insert into OHC_data (symbol, last_price, last_time) values(\'BTCUSDT{i}\', 1115, "2025-01-02 15:10:05")')
+cur.execute('select * from OHC_data')
+for row in cur.fetchall():
+    print (row)
+
+"""
+https://api.binance.com/api/v3/exchangeInfo?symbol=BNBBTC
+https://www.binance.com/api/v3/ticker/price
+"""
